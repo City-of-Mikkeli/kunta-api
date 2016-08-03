5b0f108928ec64ba9a34d4be4cb6f9a408beebb0
Heikki Kurhinen <heikki.kurhinen@gmail.com>
2016-08-03 22:58:26 +0300

    Loaded some dummy content from old webpage with rss

--- kunta-api-www/routes/index.js
+++ kunta-api-www/routes/index.js
@@ -1,7 +1,17 @@
 'use strict';
 
+var feed = require("feed-read");
+
 module.exports = function(app){
   app.get('/', function(req, res){
-    res.render('pages/index.pug');
+    feed('https://open.mikkeli.fi/rss', function(err, articles) {
+      if (err) throw err;
+        var latestNews = articles.shift();
+        console.log(latestNews);
+        res.render('pages/index.pug', {
+          latestNews: latestNews,
+          news: articles
+        });
+    });
   });
 };
