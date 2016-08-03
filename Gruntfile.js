7521e4563f4983a0abca615713f995a00695848a
Antti Leppä <antti.leppa@foyt.fi>
2016-08-03 15:51:29 +0300

    Added support for generating java client from ptv swagger file into the grunt script

--- Gruntfile.js
+++ Gruntfile.js
@@ -10,13 +10,38 @@ module.exports = function(grunt) {
       'clean-javascript': ['kunta-api-spec/languages/javascript'],
       'clean-php': ['kunta-api-spec/languages/php'],
       'clean-jaxrs': ['kunta-api-spec/languages/jaxrs-spec'],
-      'clean-jaxrs-generated-cruft': ['kunta-api-spec/languages/jaxrs-spec/src/main/java/fi/otavanopisto/kuntaapi/server/RestApplication.java']
+      'clean-jaxrs-generated-cruft': ['kunta-api-spec/languages/jaxrs-spec/src/main/java/fi/otavanopisto/kuntaapi/server/RestApplication.java'],
+      'clean-ptv-java-client': ['ptv-rest-client'],
+      'clean-ptv-java-client-cruft': [
+        'ptv-rest-client/docs', 
+        'ptv-rest-client/gradle', 
+        'ptv-rest-client/build.gradle',
+        'ptv-rest-client/build.sbt',
+        'ptv-rest-client/git_push.sh',
+        'ptv-rest-client/gradle.properties',
+        'ptv-rest-client/gradlew',
+        'ptv-rest-client/gradlew.bat',
+        'ptv-rest-client/LICENSE',
+        'ptv-rest-client/README.md',
+        'ptv-rest-client/settings.gradle',
+        'ptv-rest-client/src/test',
+        'ptv-rest-client/src/main/AndroidManifest.xml',
+        'ptv-rest-client/src/main/java/io',
+        'ptv-rest-client/src/main/java/fi/otavanopisto/ptv/auth',
+        'ptv-rest-client/src/main/java/fi/otavanopisto/ptv/*.java'
+      ],
     },
     copy: {
       'copy-jaxrs-extras': {
         src: '**',
         dest: 'kunta-api-spec/languages/jaxrs-spec/',
         cwd: 'kunta-api-spec/extras/jaxrs-spec/',
+        expand: true
+      },
+      'copy-ptv-rest-client-extras': {
+        src: '**',
+        dest: 'ptv-rest-client',
+        cwd: 'ptv-rest-client-extras',
         expand: true
       }
     },
@@ -65,13 +90,36 @@ module.exports = function(grunt) {
           --group-id fi.otavanopisto.kunta-api \
           --artifact-id kunta-api-spec \
           --template-dir kunta-api-spec/templates/jax-rs-spec \
+          --additional-properties dateLibrary=java8 \
           -o kunta-api-spec/languages/jaxrs-spec/'
+      },
+      'generate-ptv-java-client': {
+//      -i http://ptvenv.cloudapp.net:1494/swagger/v1/swagger.json \
+        command : 'java -jar kunta-api-spec/swagger-codegen-cli.jar generate \
+          -i ptv-swagger.json \
+          -l java \
+          --api-package fi.otavanopisto.ptv.client\
+          --model-package fi.otavanopisto.ptv.client.model \
+          --group-id fi.otavanopisto.ptv.rest-client \
+          --artifact-id rest-client\
+          --template-dir ptv-java-templates \
+          --library jersey2 \
+          --additional-properties dateLibrary=java8 \
+          -o ptv-rest-client/'
       },
       'install-jaxrs-spec': {
         command : 'mvn install',
         options: {
           execOptions: {
             cwd: 'kunta-api-spec/languages/jaxrs-spec/'
+          }
+        }
+      },
+      'install-ptv-java-client': {
+        command : 'mvn install',
+        options: {
+          execOptions: {
+            cwd: 'ptv-rest-client'
           }
         }
       }
@@ -89,6 +137,7 @@ module.exports = function(grunt) {
   grunt.registerTask('create-php-client', ['clean:clean-php', 'shell:generate-php-client']);
   grunt.registerTask('create-jaxrs-spec', ['clean:clean-jaxrs', 'shell:generate-jaxrs-spec', 'clean:clean-jaxrs-generated-cruft', 'copy:copy-jaxrs-extras', 'shell:install-jaxrs-spec']);
   grunt.registerTask('publish-javascript-client', ['create-javascript-client', 'publish:publish-javascript-client']);
+  grunt.registerTask('generate-ptv-java-client', ['clean:clean-ptv-java-client', 'shell:generate-ptv-java-client', 'clean:clean-ptv-java-client-cruft', 'copy:copy-ptv-rest-client-extras', 'shell:install-ptv-java-client']);
   
   grunt.registerTask('default', [ 'download-dependencies', 'create-jaxrs-spec', 'create-javascript-client', 'create-php-client' ]);
 };
---
+++ ptv-rest-client-extras/src/main/java/fi/otavanopisto/ptv/client/ApiResponse.java
@@ -0,0 +1,30 @@
+package fi.otavanopisto.ptv.client;
+
+public class ApiResponse<T> {
+
+  public ApiResponse(int status, String message, T response) {
+    this.status = status; 
+    this.response = response;
+    this.message = message;
+  }
+
+  public T getResponse() {
+    return response;
+  }
+
+  public int getStatus() {
+    return status;
+  }
+  
+  public String getMessage() {
+    return message;
+  }
+
+  public boolean isOk() {
+    return status >= 200 && status <= 299;
+  }
+
+  private int status;
+  private String message;
+  private T response;
+}
