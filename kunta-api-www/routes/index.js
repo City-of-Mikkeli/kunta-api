a05f221c8bcf16094413b831c8d641c6a93462ad
Antti Leppä <antti.leppa@foyt.fi>
2016-08-04 13:51:40 +0300

    Changed service classes to be read from the server in www-project

--- kunta-api-www/routes/index.js
+++ kunta-api-www/routes/index.js
@@ -1,16 +1,41 @@
 'use strict';
 
+var config = require(__dirname + '/../config.json');
 var feed = require("feed-read");
+var KuntaApi = require(__dirname + '/../kunta-api')({
+  basePath: config.api.basePath
+});
+var ServicesApi = new KuntaApi.ServicesApi();
 
 module.exports = function(app){
-  app.get('/', function(req, res){
-    feed('https://open.mikkeli.fi/rss', function(err, articles) {
-      if (err) throw err;
+  app.get('/', function(req, res) {
+    
+    ServicesApi.listServiceClasses(config.defaults.organizationId).then(function(apiServiceClasses) {
+      feed('https://open.mikkeli.fi/rss', function(err, articles) {
+        if (err) throw err;
+        
+        // TODO: use locale
+        
+        var serviceClasses = apiServiceClasses.map(function (apiServiceClass) {
+          return {
+            'id': apiServiceClass.id,
+            'name': apiServiceClass.name
+          };
+        });
+        
+        serviceClasses.sort(function(serviceClass1, serviceClass2) {
+          return serviceClass1.name.localeCompare(serviceClass2.name);
+        });
+        
         var latestNews = articles.shift();
         res.render('pages/index.pug', {
           latestNews: latestNews,
-          news: articles
+          news: articles,
+          serviceClasses: serviceClasses
         });
+      });
+    }, function(error) {
+      console.error(error);
     });
   });
 };
