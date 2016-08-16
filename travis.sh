7462e413d24e9c704de7a483a25266e487b68862
Antti Leppä <antti.leppa@foyt.fi>
2016-08-16 05:16:38 +0300

    Added travis conf for scanning project in SonarQube

---
+++ travis.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -euo pipefail
+
+if [ "$TRAVIS_PULL_REQUEST" != "false" ] && [ -n "${GITHUB_TOKEN}" ] && [ -n "${SONARQUBE_TOKEN}" ]; then
+    echo 'Internal pull request: trigger QA and analysis'
+
+    mvn org.jacoco:jacoco-maven-plugin:prepare-agent deploy sonar:sonar \
+        $MAVEN_OPTIONS \
+        -Dsource.skip=true \
+        -Pdeploy-sonarsource \
+        -Dsonar.projectKey=$SONAR_PROJECT_KEY \
+        -Dsonar.analysis.mode=issues \
+        -Dsonar.github.pullRequest=$TRAVIS_PULL_REQUEST \
+        -Dsonar.github.repository=$TRAVIS_REPO_SLUG \
+        -Dsonar.github.oauth=$GITHUB_TOKEN \
+        -Dsonar.host.url=$SONAR_HOST_URL \
+        -Dsonar.login=$SONAR_TOKEN
+        
+fi
