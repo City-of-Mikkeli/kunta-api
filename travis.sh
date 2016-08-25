c9a9c122ee22324f19f7901aff4f4742d1b39fd0
Antti Leppä <antti.leppa@foyt.fi>
2016-08-25 11:49:22 +0300

    Switched to sonar-runner from Maven for SonarQube analysis

--- travis.sh
+++ travis.sh
@@ -1,21 +1,13 @@
 #!/bin/bash
 set -euo pipefail
 
-#if [ "$TRAVIS_PULL_REQUEST" != "false" ] && [ -n "${GITHUB_TOKEN}" ] && [ -n "${SONARQUBE_TOKEN}" ]; then
-    export MAVEN_OPTS="-Xmx1G -Xms128m"
-    export MAVEN_OPTIONS=""
+#if [ "$TRAVIS_PULL_REQUEST" != "false" ] && [ -n "${GITHUB_TOKEN}" ] && [ -n "${SONAR_TOKEN}" ]; then
 
-    echo 'Trigger QA and analysis'
-    mvn clean verify sonar:sonar \
-        $MAVEN_OPTIONS \
-        -Dsource.skip=true \
-        -Pdeploy-sonarsource \
-        -Dsonar.projectKey=$SONAR_PROJECT_KEY \
-        -Dsonar.analysis.mode=issues \
-        -Dsonar.github.pullRequest=$TRAVIS_PULL_REQUEST \
-        -Dsonar.github.repository=$TRAVIS_REPO_SLUG \
-        -Dsonar.github.oauth=$GITHUB_TOKEN \
-        -Dsonar.host.url=$SONAR_HOST_URL \
-        -Dsonar.login=$SONAR_TOKEN
+  sh sonar-runner/bin/sonar-runner -Dsonar.host.url=$SONAR_HOST_URL \
+    -Dsonar.login=$SONAR_TOKEN \
+    -Dsonar.projectKey=$SONAR_PROJECT_KEY \
+    -Dsonar.github.oauth=$GITHUB_TOKEN \
+    -Dsonar.github.repository=$TRAVIS_REPO_SLUG \
+    -Dsonar.github.pullRequest=$TRAVIS_PULL_REQUEST
         
 #fi
