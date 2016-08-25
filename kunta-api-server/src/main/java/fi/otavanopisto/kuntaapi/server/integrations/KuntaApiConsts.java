48f610896fb4b694ff2e8079045576a8e6bb8b18
Antti Leppä <antti.leppa@foyt.fi>
2016-08-25 13:36:20 +0300

    Fixed SonarQube warnings

--- kunta-api-server/src/main/java/fi/otavanopisto/kuntaapi/server/integrations/KuntaApiConsts.java
+++ kunta-api-server/src/main/java/fi/otavanopisto/kuntaapi/server/integrations/KuntaApiConsts.java
@@ -1,8 +1,17 @@
 package fi.otavanopisto.kuntaapi.server.integrations;
 
+/**
+ * Contains consts for Kunta API
+ * 
+ * @author Otavan Opisto
+ */
 public class KuntaApiConsts {
 
   public static final String IDENTIFIER_NAME = "KA";
+  
   public static final String DESCRIPTION_TYPE_ENRICHED = "Enriched";
+  
+  private KuntaApiConsts() {
+  }
   
 }
