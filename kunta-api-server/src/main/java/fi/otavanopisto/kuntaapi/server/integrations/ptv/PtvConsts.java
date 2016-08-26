9033ad733455872068cbfa85a019557b399845ae
Antti Leppä <antti.leppa@foyt.fi>
2016-08-26 07:28:53 +0300

    Fixed SonarQube issues on PtvConsts

--- kunta-api-server/src/main/java/fi/otavanopisto/kuntaapi/server/integrations/ptv/PtvConsts.java
+++ kunta-api-server/src/main/java/fi/otavanopisto/kuntaapi/server/integrations/ptv/PtvConsts.java
@@ -1,13 +1,22 @@
 package fi.otavanopisto.kuntaapi.server.integrations.ptv;
 
+/**
+ * Constants for PTV
+ * 
+ * @author Otavan Opisto
+ */
+@SuppressWarnings ("squid:S1125")
 public class PtvConsts {
 
   public static final String DEFAULT_LANGUAGE = "fi";
   public static final String IDENTIFIFER_NAME = "PTV";
   public static final boolean SYNCHRONIZE = true;
-  public static final boolean SYNCHRONIZE_ORGANIZATIONS = SYNCHRONIZE && true;
+  public static final boolean SYNCHRONIZE_ORGANIZATIONS = SYNCHRONIZE && false;
   public static final boolean SYNCHRONIZE_ORGANIZATION_SERVICES = SYNCHRONIZE && true;
-  public static final boolean SYNCHRONIZE_GENERAL_DESCRIPTIONS = SYNCHRONIZE && true;
+  public static final boolean SYNCHRONIZE_GENERAL_DESCRIPTIONS = SYNCHRONIZE && false;
   public static final boolean SYNCHRONIZE_SERVICES = SYNCHRONIZE && false;
+  
+  private PtvConsts() {
+  }
   
 }
