83ffbeaa25ce3caf434bffcd02a30d0f926e9f06
Antti Leppä <antti.leppa@foyt.fi>
2016-09-08 09:52:44 +0300

    Added support to disable MWP cache

--- kunta-api-server/src/main/java/fi/otavanopisto/kuntaapi/server/integrations/mwp/MwpConsts.java
+++ kunta-api-server/src/main/java/fi/otavanopisto/kuntaapi/server/integrations/mwp/MwpConsts.java
@@ -7,9 +7,10 @@ package fi.otavanopisto.kuntaapi.server.integrations.mwp;
  */
 public class MwpConsts {
   
-  public static final String IDENTIFIFER_NAME = "MWP";
-  public static final boolean SYNCHRONIZE = true;
+  public static final String IDENTIFIER_NAME = "MWP";
+  public static final boolean SYNCHRONIZE = false;
   public static final String CACHE_NAME = "mwp";
+  public static final boolean CACHE_RESPONSES = false;
 
   private MwpConsts() {
   }
