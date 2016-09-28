ae13718c71836029a907861148e6b43a7c6ff150
Antti Leppä <antti.leppa@metatavu.fi>
2016-09-28 12:50:27 +0300

    Generated source

--- kunta-api-spec/languages/jaxrs-spec/src/main/java/fi/otavanopisto/kuntaapi/server/rest/OrganizationsApi.java
+++ kunta-api-spec/languages/jaxrs-spec/src/main/java/fi/otavanopisto/kuntaapi/server/rest/OrganizationsApi.java
@@ -33,7 +33,7 @@ import java.util.List;
 @Api(description = "the organizations API")
 @Consumes({ "application/json;charset=utf-8" })
 @Produces({ "application/json;charset=utf-8" })
-@javax.annotation.Generated(value = "class io.swagger.codegen.languages.JavaJAXRSSpecServerCodegen", date = "2016-09-28T12:48:43.462+03:00")
+@javax.annotation.Generated(value = "class io.swagger.codegen.languages.JavaJAXRSSpecServerCodegen", date = "2016-09-28T12:50:18.610+03:00")
 
 public abstract class OrganizationsApi extends AbstractApi {
 
